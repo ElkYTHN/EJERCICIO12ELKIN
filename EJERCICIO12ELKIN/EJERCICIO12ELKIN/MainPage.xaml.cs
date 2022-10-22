6c702154707ed6cc17ca5a020860980b357f5a0d
ElkYTHN <elkinportillo4@gmail.com>
2022-10-21 19:52:18 -0600

    Agregar archivos de proyecto.

---
+++ EJERCICIO12ELKIN/EJERCICIO12ELKIN/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EJERCICIO12ELKIN
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            var emple = new Models.Empleados
+            {
+                nombre = txtNombre.Text,
+                apellidos = txtApellidos.Text,
+                edad = txtEdad.Text,
+                email = txtEmail.Text,
+            };
+
+            var datos = new PageData();
+            datos.BindingContext = emple;
+            await Navigation.PushAsync(datos);
+
+        }
+    }
+}
---
+++ EJERCICIO12ELKIN/EJERCICIO12ELKIN/Models/Empleados.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EJERCICIO12ELKIN.Models
+{
+    internal class Empleados
+    {
+
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string edad { get; set; }
+        public string email { get; set; }
+
+    }
+}
